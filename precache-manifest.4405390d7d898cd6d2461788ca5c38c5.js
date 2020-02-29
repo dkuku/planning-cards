3798ff9d8fe4bd8c6605339519230a0bc0510ef6
Daniel Kukula <daniel.kuku@gmail.com>
2020-02-29 23:11:13 +0000

    Updates

---
+++ precache-manifest.4405390d7d898cd6d2461788ca5c38c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "812b0f98b0521ac9bd50a7089c70f040",
+    "url": "/planning-cards/index.html"
+  },
+  {
+    "revision": "3a66c020d13c84c8aa69",
+    "url": "/planning-cards/static/css/main.c6bb8aa7.chunk.css"
+  },
+  {
+    "revision": "d55fe56e23d2fe4ec530",
+    "url": "/planning-cards/static/js/2.1134e2f4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/planning-cards/static/js/2.1134e2f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a66c020d13c84c8aa69",
+    "url": "/planning-cards/static/js/main.5ae3fa7f.chunk.js"
+  },
+  {
+    "revision": "b6d829b0dde6b443b82e",
+    "url": "/planning-cards/static/js/runtime-main.f3d37f2e.js"
+  }
+]);
